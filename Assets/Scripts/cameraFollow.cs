44d4f13f10333a15b1085899f54883bc72539c5c
James Tiernan <Tiernan-J1@ulster.ac.uk>
2025-11-03 10:30:11 +0000

    smooth camera

--- Assets/Scripts/cameraFollow.cs
+++ Assets/Scripts/cameraFollow.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class cameraFollow : MonoBehaviour
@@ -17,7 +18,12 @@ public class cameraFollow : MonoBehaviour
     {
         if (target != null)
         {
-             transform.position = new Vector3 (target.transform.position.x + offset.x, target.transform.position.y + offset.y,-10);
+            /*
+            Vector2 mouse_position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            offset.x = mouse_position.x * 10f;
+            offset.y = mouse_position.y * 2f;
+            */
+            transform.position = Vector3.Lerp(transform.position, new Vector3 (target.transform.position.x + offset.x, target.transform.position.y + offset.y,-10),.01f);
         }
     }
 }
