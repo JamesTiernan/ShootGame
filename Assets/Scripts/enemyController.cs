0fd5a5e4a817529302349c1e445682ab282fac92
James Tiernan <Tiernan-J1@ulster.ac.uk>
2025-11-17 10:27:28 +0000

    stuff

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.U2D.IK;
 
 public class enemyController : MonoBehaviour
 {
@@ -6,25 +7,35 @@ public class enemyController : MonoBehaviour
     [SerializeField] GameObject leftArm;
     [SerializeField] GameObject rightArm;
     [SerializeField] GameObject weapon;
+    bool checkFront;
 
-    //LimbSolver2D armL;
-    //LimbSolver2D armR;
+    LimbSolver2D armL;
+    LimbSolver2D armR;
+    Transform oldTargetL;
+    Transform oldTargetR;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //armL = leftArm.GetComponent<LimbSolver2D>();
-        //armR = rightArm.GetComponent<LimbSolver2D>();
+        armL = leftArm.GetComponent<LimbSolver2D>();
+        armR = rightArm.GetComponent<LimbSolver2D>();
+        //oldTargetL = armL.target;
+        //oldTargetR = armR.target;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        checkFront = Physics2D.OverlapBox(new Vector2(transform.position.x + (0.4f * transform.localScale.x), transform.position.y - 0.2f), new Vector2(0.5f, .5f), 0f, player.LayerMask);
+        
+        checkFront = Physics2D.OverlapBox(new Vector2(transform.position.x + (0.4f * transform.localScale.x), transform.position.y - 0.2f), new Vector2(0.5f, .5f), 0f);
         if (checkFront)
         {
-            armL.target = weapon;
-            armR.target = weapon;
-        }*/
+            //armL.target = weapon;
+            //armR.target = weapon;
+        }
+        else
+        {
+            //armL.target = oldTargetL;
+            //armR.target = oldTargetR;
+        }
     }
 }
